56668f606e53b120745a97e07723962f87a257e1
mauriciabad <hello@mauriciabad.com>
2019-04-04 15:02:08 +0000

    Fix user db

--- db/schema.rb
+++ db/schema.rb
@@ -34,9 +34,14 @@ ActiveRecord::Schema.define(version: 20190401174735) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
     t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "oauth_token"
+    t.datetime "oauth_expires_at"
   end
 
 end
