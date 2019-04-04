a144ab6c795273c579bd4325d7dc25ccd2fd70cb
raimonmerce <raimon.merc.gots@gmail.com>
2019-04-04 14:33:24 +0000

    Login v2.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
 
   resources :sessions, only: [:create, :destroy]
   resource :home, only: [:show]
-=======
-  
-  resources :comments
-  resources :users
->>>>>>> refs/remotes/origin/master
+
   resources :issues
   resources :users
   
