f8024e0a51c4602feca5ddc848840dd3b6b556f3
mauriciabad <hello@mauriciabad.com>
2019-04-09 10:27:01 +0000

    If not logged in it takes you to the login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
+
+
 class ApplicationController < ActionController::Base
+  before_action :require_login
+
+  private
+  
+    def require_login
+      unless !defined?(current_user)
+        redirect_to "/"
+      end
+    end
   protect_from_forgery with: :exception
   helper_method :current_user
 
