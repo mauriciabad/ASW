3677f30327459611f5a500a593b9dc22b9dd3f93
mauriciabad <hello@mauriciabad.com>
2019-04-09 10:32:43 +0000

    Login semifix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,15 @@ class ApplicationController < ActionController::Base
   before_action :require_login
 
   private
-  
     def require_login
-      unless !defined?(current_user)
+      unless !defined?(current_user) or current_user
         redirect_to "/"
       end
     end
   protect_from_forgery with: :exception
   helper_method :current_user
 
- 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
