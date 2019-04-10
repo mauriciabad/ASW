62315bcd482edb3e82fbdcb5602e66785d366e28
tataslas <sandrafh@otmail.es>
2019-04-10 21:07:26 +0000

    ordenacion de columnas

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -47,6 +47,23 @@ class IssuesController < ApplicationController
     #watching
     elsif params[:watching] == "me"
       @issues = Issue.joins(:watches).where(['watches.issue_id = issues.id AND watches.user_id = ?', current_user.id])
+    #order by
+    elsif params[:title] == "order"
+      @issues = Issue.order(:title)
+    elsif params[:type] == "order"
+      @issues = Issue.order(:type)
+    elsif params[:priority] == "order"
+      @issues = Issue.order(:priority)
+    elsif params[:status] == "order"
+      @issues = Issue.order(:status)
+    elsif params[:votesCount] == "order"
+      @issues = Issue.order(:votesCount)
+    elsif params[:assigned_user] == "order"
+      @issues = Issue.order(:assigned_user)
+    elsif params[:created_at] == "order"
+      @issues = Issue.order(:created_at)
+    elsif params[:updated_at] == "order"
+      @issues = Issue.order(:updated_at)
     #all
     else
       @issues = Issue.all
@@ -74,7 +91,7 @@ class IssuesController < ApplicationController
   # POST /issues.json
   def create
     @issue = Issue.new(issue_params)
-    # @issue.assigned_user = current_user.name
+    #@issue.assigned_user = current_user.name
     @issue.user_id = current_user.id
     @issue.votesCount = 0
     respond_to do |format|
