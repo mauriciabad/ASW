ff6f20f3ac66496916338c086352143f9b01b932
mauriciabad <hello@mauriciabad.com>
2019-04-10 01:34:51 +0000

    Heroku 6

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,6 @@
 require_relative 'boot'
 
 require 'rails/all'
-    config.assets.initialize_on_precompile = false
-
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -12,7 +10,6 @@ module HelloApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
     config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
