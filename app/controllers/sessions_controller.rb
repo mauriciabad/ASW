715612145f578e679270bac56e10e3bc086c34c4
tataslas <sandrafh@otmail.es>
2019-05-13 21:40:36 +0000

    gets users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,9 @@
 class SessionsController < ApplicationController
   def create
+    #guardem a auth el que rebem de la API quan fem login
+    auth = request.env["omniauth.auth"]
+    session[:omniauth] = auth.except('extra')
+      
     user = User.from_omniauth(request.env["omniauth.auth"])
     session[:user_id] = user.id
     redirect_to issues_path
