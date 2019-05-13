a9d5246e5091ffecf3fb97f982174842cf778e23
Xavier <ec2-user@ip-172-31-27-114.us-east-2.compute.internal>
2019-05-13 16:59:57 +0000

    swagger comments done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,12 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
-
+  before_action :set_issue
+  
   # GET /comments
   # GET /comments.json
   def index
-    @comments = Comment.all
-
+    # @comments = Comment.all
+    @comments = @issue.comment.all
     respond_to do |format|
       format.html
       format.json { render json: @comments, status: :ok }
@@ -34,21 +35,21 @@ class CommentsController < ApplicationController
   # POST /comments.json
   def create
     @comment = Comment.new(comment_params)
-    @comment.save
-    redirect_to @comment.issue
-        respond_to do |format|
-      format.html
-      format.json { render json: @comment, status: :ok }
+    # @comment.save
+    # redirect_to @comment.issue
+    #     respond_to do |format|
+    #   format.html
+    #   format.json { render json: @comment, status: :ok }
+    # end
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @comment.issue  }
+        format.json { render json: @comment, status: :created }
+      else
+        format.html { render :new }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
     end
-#    respond_to do |format|
-#      if comment.save
-#        format.html { redirect_to comment, notice: 'Comment was successfully created.' }
-#        format.json { render json: comment, status: :created }
-#      else
-#        format.html { render :new }
-#        format.json { render json: comment.errors, status: :unprocessable_entity }
-#      end
-#    end
     
   end
 
@@ -58,13 +59,12 @@ class CommentsController < ApplicationController
     
     # logger.debug "Person attributes hash: #{comment_params[:text]}"
     # logger.debug "Person attributes hash: #{@comment.text}"
-    @comment.update(comment_params)
+    # @comment.update(comment_params)
     
-    redirect_to @comment.issue
     
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
+        format.html { redirect_to  @comment.issue }
         format.json { render json: @comment, status: :ok }
       else
         format.html { render :edit }
@@ -100,4 +100,17 @@ class CommentsController < ApplicationController
     def comment_params
       params.require(:comment).permit(:text, :issue_id, :user_id)
     end
+    
+    def set_issue
+        @issue = Issue.find_by(id: params[:issue_id])
+        if @issue.nil?
+           respond_to do |format|
+            format.html
+            format.json { render json: { error: 'Issue not found' },
+                              status: :not_found }
+            end
+        end
+    end
+    
 end
+
