5780e01893a71476f569c37b1f5a17fca9b33046
Maurici Abad Gutierrez <hello@mauriciabad.com>
2019-11-06 22:22:40 +0100

    Delete Issue when creator is deleted

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,5 +1,5 @@
 class Issue < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, on_delete: :cascade
   # belongs_to :assigned_user, :class_name => "User", foreign_key: "user_id"
   has_many :comment, dependent: :destroy
   has_many :votes, dependent: :destroy
